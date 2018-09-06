15f3cf21156f44c2bb37e50d47d8475efd99d899
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-09-06 18:08:28 -0300

    Initial Commit

---
+++ XCServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCServer
+//
+//  Created by Rodrigo Vianna on 06/09/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ XCServerTests/XCServerTests.swift
@@ -0,0 +1,36 @@
+//
+//  XCServerTests.swift
+//  XCServerTests
+//
+//  Created by Rodrigo Vianna on 06/09/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import XCTest
+@testable import XCServer
+
+class XCServerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
